262c2b74cfe6ffdf9c9ae92d9b34aca9d662eb59
Richard <Richardlife101@gmail.com>
2025-10-24 10:40:50 -0400

    done 31

--- Automaton.java
+++ Automaton.java
@@ -31,12 +31,7 @@ public class Automaton
     public void print()
     {
         for(int cellValue : state) {
-            if(cellValue == 1) {
-                System.out.print("*");
-            }
-            else {
-                System.out.print(" ");
-            }
+            System.out.print(cellValue == 1 ? '*' : ' ');
         }
         System.out.println();
     }   
@@ -51,22 +46,12 @@ public class Automaton
         // Naively update the state of each cell
         // based on the state of its two neighbors.
         for(int i = 0; i < state.length; i++) {
-            int left, center, right;
-            if(i == 0) {
-                left = 0;
-            }
-            else {
-                left = state[i - 1];
-            }
+            int right, center, left;
+            right = (i==0 ? 0 : state[i - i]);
             center = state[i];
-            if(i + 1 < state.length) {
-                right = state[i + 1];
-            }
-            else {
-                right = 0;
-            }
-            nextState[i] = (left + center + right) % 2;
-        }
+            left = (i + 1 < state.length ?  state[i + 1] :  0); 
+            nextState[i] = (right + center + left) % 2;
+        } 
         state = nextState;
     }
     
